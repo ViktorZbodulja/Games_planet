ae14e9cdad872cea9817510ec14f9d68781dfa83
Viktor Zbodulja <viktorzbodulja@gmail.com>
2023-11-22 21:52:26 +0100

    api key changed

--- src/api.js
+++ src/api.js
@@ -28,19 +28,19 @@ const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 //Popular games
 //Fetching from last year to current date
-const popular_games = `games?key=9d16d58232514c0ba94d7c106c21e2ee&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
-const upcoming_games = `games?key=9d16d58232514c0ba94d7c106c21e2ee&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
-const new_games = `games?key=9d16d58232514c0ba94d7c106c21e2ee&dates=${lastYear},${currentDate}&ordering=-added&page_size=10`;
+const popular_games = `games?key=3d47e9c894c049e0aa8a3715acbdccd6&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?key=3d47e9c894c049e0aa8a3715acbdccd6&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const new_games = `games?key=3d47e9c894c049e0aa8a3715acbdccd6&dates=${lastYear},${currentDate}&ordering=-added&page_size=10`;
 //released
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${new_games}`;
 //GAME DETAILS
 export const gameDetailsURL = (game_id) =>
-  `${base_url}games/${game_id}.json?&key=9d16d58232514c0ba94d7c106c21e2ee`;
+  `${base_url}games/${game_id}.json?&key=3d47e9c894c049e0aa8a3715acbdccd6`;
 //Game screenshots
 export const gameScreenshotURL = (game_id) =>
-  `${base_url}games/${game_id}/screenshots?&key=9d16d58232514c0ba94d7c106c21e2ee`;
+  `${base_url}games/${game_id}/screenshots?&key=3d47e9c894c049e0aa8a3715acbdccd6`;
 //Searched Game
 export const searchedGameURL = (game_name) =>
-  `${base_url}games?key=9d16d58232514c0ba94d7c106c21e2ee&search=${game_name}&page_size=10`;
+  `${base_url}games?key=3d47e9c894c049e0aa8a3715acbdccd6&search=${game_name}&page_size=10`;
