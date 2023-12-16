e142d7f8f4549ba2e2b3ab91add516420c75b585
Viktor Zbodulja <viktorzbodulja@gmail.com>
2023-12-16 19:03:39 +0100

    optimised mobile performance

--- src/components/Game.js
+++ src/components/Game.js
@@ -22,38 +22,59 @@ function Game({ name, released, image, id, stores }) {
   const releaseDate = new Date(released);
   const formattedDate = format(releaseDate, "dd.MM.yyyy");
 
+  //for mobile
+  const isMobile = window.innerWidth < 768;
+
   return (
-    <motion.div
-      className="game"
-      onClick={loadDetailHandler}
-      layoutId={stringPathId}
-      variants={popup}
-      initial="hidden"
-      animate="show">
-      <Link to={`/game/${id}`}>
-        <div className="info_container">
-          <motion.h3 layoutId={`title ${stringPathId}`}>{name}</motion.h3>
-          <h4>Release date: {formattedDate}</h4>
-        </div>
-        <motion.div
-          className="store_container"
-          layoutId={`store ${stringPathId}`}>
-          {stores &&
-            stores.slice(0, 3).map((store) => {
-              return (
+    <Link to={`/game/${id}`}>
+      {isMobile ? (
+        // If on mobile, no motion animation
+        <div className="game" onClick={loadDetailHandler}>
+          <div className="info_container">
+            <h3>{name}</h3>
+            <h4>Release date: {formattedDate}</h4>
+          </div>
+          <div className="store_container">
+            {stores &&
+              stores.slice(0, 3).map((store) => (
                 <div className="store" key={store.store.id}>
                   {store.store.name}
                 </div>
-              );
-            })}
+              ))}
+          </div>
+          <img src={resizeImage(image, 640)} alt={name} />
+        </div>
+      ) : (
+        // If not on mobile
+        <motion.div
+          className="game"
+          onClick={loadDetailHandler}
+          layoutId={stringPathId}
+          variants={popup}
+          initial="hidden"
+          animate="show">
+          <div className="info_container">
+            <motion.h3 layoutId={`title ${stringPathId}`}>{name}</motion.h3>
+            <h4>Release date: {formattedDate}</h4>
+          </div>
+          <motion.div
+            className="store_container"
+            layoutId={`store ${stringPathId}`}>
+            {stores &&
+              stores.slice(0, 3).map((store) => (
+                <div className="store" key={store.store.id}>
+                  {store.store.name}
+                </div>
+              ))}
+          </motion.div>
+          <motion.img
+            src={resizeImage(image, 640)}
+            alt={name}
+            layoutId={`image ${stringPathId}`}
+          />
         </motion.div>
-        <motion.img
-          src={resizeImage(image, 640)}
-          alt={name}
-          layoutId={`image ${stringPathId}`}
-        />
-      </Link>
-    </motion.div>
+      )}
+    </Link>
   );
 }
 
